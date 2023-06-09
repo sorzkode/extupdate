02347cfdd56eeb9387cee878a6eb0071c4d72a3d
RILEYJITSU <rileyjitsu@gmail.com>
2023-06-09 13:40:52 -0400

    general updates

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='extupdate',
     version='1.0.0',
     description='EXTUPDATE',
     url='https://github.com/sorzkode/',
-    author='sorzkode',
+    author='Mister Riley',
     author_email='<sorzkode@proton.me>',
     packages=setuptools.find_packages(),
-    install_requires=['PySimpleGUI', 'tkinter'],
-    long_description='Quickly update/change Excel file extensions.',
+    install_requires=['PySimpleGUI'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        ],
+    ],
+    license='MIT',
 )
